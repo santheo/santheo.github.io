0544a5a7ca40e2157404ff883752eb62f3a45f95
Sandor Weisz <santheo@mac.com>
2013-11-11 16:58:05 -0600

    Added config.rb to support Compass.

---
+++ config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = ""
+sass_dir = ""
+images_dir = ""
+javascripts_dir = ""
+fonts_dir = ""
